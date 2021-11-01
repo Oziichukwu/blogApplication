51d6881b832ed076efcbb627b586157c8390847f
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-01 15:35:59 +0100

    blog application

--- src/main/java/africa/semicolon/blogApplication/data/models/Comment.java
+++ src/main/java/africa/semicolon/blogApplication/data/models/Comment.java
@@ -1,9 +1,13 @@
 package africa.semicolon.blogApplication.data.models;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
 public class Comment {
 
+    private String commentUniqueId;
     private String commentorName;
     private String content;
     private LocalDate dateCreated;
