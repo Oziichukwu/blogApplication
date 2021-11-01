61e614aeb20b5350de7f68bf3deab0028ea3bd11
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-01 09:47:26 +0100

    repository

--- src/main/java/africa/semicolon/blogApplication/data/repositories/AuthorRepository.java
+++ src/main/java/africa/semicolon/blogApplication/data/repositories/AuthorRepository.java
@@ -1,2 +1,14 @@
-package africa.semicolon.blogApplication.data.repositories;public class AuthorRepository {
+package africa.semicolon.blogApplication.data.repositories;
+
+import africa.semicolon.blogApplication.data.models.Author;
+
+import java.util.List;
+
+public interface AuthorRepository {
+
+    Author save (Author author);
+    Author findByAuthorId(String id);
+    void delete(Author author);
+    void delete(String id);
+    List<Author>findAll();
 }
