3245f4d38e6c5556be9adf8f05d809426f6ba107
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-01 15:34:47 +0100

    comment repository

--- src/main/java/africa/semicolon/blogApplication/data/repositories/CommentRepositoryImpl.java
+++ src/main/java/africa/semicolon/blogApplication/data/repositories/CommentRepositoryImpl.java
@@ -1,2 +1,44 @@
-package africa.semicolon.blogApplication.data.repositories;public class CommentRepositoryImpl {
+package africa.semicolon.blogApplication.data.repositories;
+
+import africa.semicolon.blogApplication.data.models.Comment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommentRepositoryImpl implements CommentRepository{
+    List<Comment> comments = new ArrayList<>();
+
+    @Override
+    public Comment save(Comment comment) {
+        comments.add(comment);
+        return findByUniqueId(comment.getCommentUniqueId());
+    }
+
+    @Override
+    public Comment findByUniqueId(String id) {
+
+        for ( Comment comment : comments){
+
+            if (comment.getCommentUniqueId().equalsIgnoreCase(id)){
+                return comment;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void delete(Comment comment) {
+        comments.remove(comment);
+    }
+
+    @Override
+    public void delete(String id) {
+        Comment comment = findByUniqueId(id);
+        comments.remove(comment);
+    }
+
+    @Override
+    public List<Comment> findAll() {
+        return comments;
+    }
 }
