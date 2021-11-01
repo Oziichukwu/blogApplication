4fdfdd0027ddebec896b3cc7625c76c2b95718ae
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-01 13:21:49 +0100

    blog application

--- src/main/java/africa/semicolon/blogApplication/data/models/Post.java
+++ src/main/java/africa/semicolon/blogApplication/data/models/Post.java
@@ -1,13 +1,15 @@
 package africa.semicolon.blogApplication.data.models;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class Post {
-
     private String content;
     private String title;
-    private long id;
+    private String PostId;
     private Author author;
     private List<Comment> comments  = new ArrayList<>();
 
