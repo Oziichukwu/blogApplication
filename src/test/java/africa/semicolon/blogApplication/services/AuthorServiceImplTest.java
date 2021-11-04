220831f6e41bd96b7367838579ee7512397ca489
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-04 14:18:59 +0100

    blog updated

--- src/test/java/africa/semicolon/blogApplication/services/AuthorServiceImplTest.java
+++ src/test/java/africa/semicolon/blogApplication/services/AuthorServiceImplTest.java
@@ -1,4 +1,41 @@
+package africa.semicolon.blogApplication.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AuthorServiceImplTest {
-  
+
+    AuthorService authorService;
+    @BeforeEach
+    void setUp() {
+        authorService = new AuthorServiceImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createAuthor() {
+
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void deleteAuthorById() {
+    }
+
+    @Test
+    void deleteAuthor() {
+    }
+
+    @Test
+    void findAuthorById() {
+    }
 }
--- src/test/java/africa/semicolon/blogApplication/services/PostServiceImplTest.java
+++ src/test/java/africa/semicolon/blogApplication/services/PostServiceImplTest.java
@@ -1,4 +1,45 @@
+package africa.semicolon.blogApplication.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PostServiceImplTest {
-  
+
+    PostService postService;
+    @BeforeEach
+    void setUp() {
+        postService = new PostServiceImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createPost() {
+
+    }
+
+    @Test
+    void findAByPost() {
+    }
+
+    @Test
+    void updatePost() {
+    }
+
+    @Test
+    void findPostById() {
+    }
+
+    @Test
+    void deletePostById() {
+    }
+
+    @Test
+    void addCommentToPost() {
+    }
 }
