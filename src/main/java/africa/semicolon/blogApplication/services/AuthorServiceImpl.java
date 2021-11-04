02742e1942fa91f9bd70edb3584022e93343f954
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-04 14:20:07 +0100

    blog updated

--- src/main/java/africa/semicolon/blogApplication/services/AuthorServiceImpl.java
+++ src/main/java/africa/semicolon/blogApplication/services/AuthorServiceImpl.java
@@ -1,2 +1,44 @@
-package africa.semicolon.blogApplication.services;public class AuthorServiceImpl {
+package africa.semicolon.blogApplication.services;
+
+import africa.semicolon.blogApplication.data.dtos.request.AuthorRequestDto;
+import africa.semicolon.blogApplication.data.dtos.response.AuthorResponseDto;
+import africa.semicolon.blogApplication.data.models.Author;
+import africa.semicolon.blogApplication.data.repositories.AuthorRepository;
+import africa.semicolon.blogApplication.data.repositories.AuthorRepositoryImpl;
+
+import java.util.List;
+
+public class AuthorServiceImpl implements AuthorService {
+
+    private final AuthorRepository authorRepository = new AuthorRepositoryImpl();
+
+    @Override
+    public AuthorResponseDto createAuthor(AuthorRequestDto authorRequestDto) {
+        return null;
+    }
+
+    @Override
+    public AuthorResponseDto updateAuthor(AuthorRequestDto authorRequestDto) {
+        return null;
+    }
+
+    @Override
+    public List<Author> findById() {
+        return null;
+    }
+
+    @Override
+    public void deleteAuthorById(String id) {
+
+    }
+
+    @Override
+    public void deleteAuthor(Author author) {
+
+    }
+
+    @Override
+    public Author findAuthorById(String id) {
+        return null;
+    }
 }
--- src/main/java/africa/semicolon/blogApplication/services/PostService.java
+++ src/main/java/africa/semicolon/blogApplication/services/PostService.java
@@ -1,2 +1,18 @@
-package africa.semicolon.blogApplication.services;public class PostService {
+package africa.semicolon.blogApplication.services;
+
+import africa.semicolon.blogApplication.data.dtos.request.PostRequestDto;
+import africa.semicolon.blogApplication.data.dtos.response.PostResponseDto;
+import africa.semicolon.blogApplication.data.exceptions.PostDoesNotExistException;
+import africa.semicolon.blogApplication.data.models.Post;
+
+import java.util.List;
+
+public interface PostService {
+
+   PostResponseDto createPost(PostRequestDto postRequestDto);
+   List<Post>findAByPost(PostRequestDto postRequestDto);
+   PostResponseDto updatePost(PostRequestDto postRequestDto);
+   Post findPostById(Long id) throws PostDoesNotExistException;
+   void deletePostById(long id);
+
 }
