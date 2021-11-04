38a8b85389928a10604fa074cdfdd0327ad77644
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-04 14:28:25 +0100

    updated blog

--- src/main/java/africa/semicolon/blogApplication/data/models/Post.java
+++ src/main/java/africa/semicolon/blogApplication/data/models/Post.java
@@ -2,6 +2,7 @@ package africa.semicolon.blogApplication.data.models;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +11,7 @@ public class Post {
     private String content;
     private String title;
     private String PostId;
+    private LocalDateTime timePublished;
     private Author author;
     private List<Comment> comments  = new ArrayList<>();
 
--- src/main/java/africa/semicolon/blogApplication/services/AuthorService.java
+++ src/main/java/africa/semicolon/blogApplication/services/AuthorService.java
@@ -1,2 +1,17 @@
-package africa.semicolon.blogApplication.services;public interface AuthorService {
+package africa.semicolon.blogApplication.services;
+
+import africa.semicolon.blogApplication.data.dtos.request.AuthorRequestDto;
+import africa.semicolon.blogApplication.data.dtos.response.AuthorResponseDto;
+import africa.semicolon.blogApplication.data.models.Author;
+
+import java.util.List;
+
+public interface AuthorService {
+
+    AuthorResponseDto createAuthor(AuthorRequestDto authorRequestDto);
+    AuthorResponseDto updateAuthor(AuthorRequestDto authorRequestDto);
+    List<Author>findById();
+    void deleteAuthorById(String id);
+    void deleteAuthor(Author author);
+    Author findAuthorById(String id);
 }
