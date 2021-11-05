2070312eba4395954f03fe9b5c8863372e7b4f1a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-05 04:25:55 +0100

    blog app

--- src/main/java/africa/semicolon/blogApplication/utils/AuthorModelMapper.java
+++ src/main/java/africa/semicolon/blogApplication/utils/AuthorModelMapper.java
@@ -1,2 +1,31 @@
-package africa.semicolon.blogApplication.utils;public class AuthorModelMapper {
+package africa.semicolon.blogApplication.utils;
+
+import africa.semicolon.blogApplication.data.dtos.request.AuthorRequestDto;
+import africa.semicolon.blogApplication.data.dtos.response.AuthorResponseDto;
+import africa.semicolon.blogApplication.data.models.Author;
+
+public class AuthorModelMapper {
+
+    public static Author map(AuthorRequestDto authorRequestDto){
+
+        Author author = new Author();
+
+        author.setAuthorId(authorRequestDto.getAuthorId());
+        author.setUserName(authorRequestDto.getUserName());
+        author.setFirstName(authorRequestDto.getFirstName());
+        author.setLastName(authorRequestDto.getUserName());
+        author.setProfession(authorRequestDto.getProfession());
+        author.setPhoneNumber(authorRequestDto.getPhoneNumber());
+        return author;
+    }
+
+    public static AuthorResponseDto map(Author savedAuthor){
+
+        AuthorResponseDto response = new AuthorResponseDto();
+        response.setAuthorId(savedAuthor.getAuthorId());
+        response.setFirstName(savedAuthor.getFirstName());
+        response.setUserName(savedAuthor.getUserName());
+        response.setEmail(savedAuthor.getEmail());
+        return response;
+    }
 }
--- src/main/java/africa/semicolon/blogApplication/utils/CommentModelMapper.java
+++ src/main/java/africa/semicolon/blogApplication/utils/CommentModelMapper.java
@@ -1,2 +1,31 @@
-package africa.semicolon.blogApplication.utils;public class CommentModelMapper {
+package africa.semicolon.blogApplication.utils;
+
+import africa.semicolon.blogApplication.data.dtos.request.CommentRequestDto;
+import africa.semicolon.blogApplication.data.dtos.response.CommentResponseDto;
+import africa.semicolon.blogApplication.data.models.Comment;
+
+public class CommentModelMapper {
+
+    public static Comment map(CommentRequestDto commentRequestDto){
+
+        Comment comment = new Comment();
+        comment.setCommentId(commentRequestDto.getCommentId());
+        comment.setCommentorName(commentRequestDto.getCommentorName());
+        comment.setContent(commentRequestDto.getContent());
+        comment.setDateCommentWasCreated(commentRequestDto.getDateCommentWasMade());
+
+        return comment;
+    }
+
+    public static CommentResponseDto map(Comment savedComment){
+
+        CommentResponseDto commentResponseDto = new CommentResponseDto();
+
+        commentResponseDto.setCommentId(savedComment.getCommentId());
+        commentResponseDto.setCommentorName(savedComment.getCommentorName());
+        commentResponseDto.setContent(savedComment.getContent());
+        commentResponseDto.setDateCommentWasMade(savedComment.getDateCommentWasCreated());
+
+        return commentResponseDto;
+    }
 }
