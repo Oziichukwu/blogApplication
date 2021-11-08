a54446ccbab636ca0633a21767196e77fcea70c4
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-08 02:39:57 +0100

    blog update

--- src/main/java/africa/semicolon/blogApplication/services/PostService.java
+++ src/main/java/africa/semicolon/blogApplication/services/PostService.java
@@ -2,17 +2,13 @@ package africa.semicolon.blogApplication.services;
 
 import africa.semicolon.blogApplication.data.dtos.request.PostRequestDto;
 import africa.semicolon.blogApplication.data.dtos.response.PostResponseDto;
-import africa.semicolon.blogApplication.data.exceptions.PostDoesNotExistException;
 import africa.semicolon.blogApplication.data.models.Post;
-
-import java.util.List;
 
 public interface PostService {
 
    PostResponseDto createPost(PostRequestDto postRequestDto);
-   List<Post>findAByPost(PostRequestDto postRequestDto);
-   PostResponseDto updatePost(PostRequestDto postRequestDto);
+   Post updatePost(String id, PostRequestDto postRequestDto);
    Post findPostById(String id);
-   void deletePostById(long id);
+   void deletePostById(String id);
 
 }
