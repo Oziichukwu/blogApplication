243d1183c45c44f56391b16eb1d718d0d2d481ec
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-08 02:39:25 +0100

    blog update

--- src/main/java/africa/semicolon/blogApplication/services/PostServiceImpl.java
+++ src/main/java/africa/semicolon/blogApplication/services/PostServiceImpl.java
@@ -2,16 +2,10 @@ package africa.semicolon.blogApplication.services;
 
 import africa.semicolon.blogApplication.data.dtos.request.PostRequestDto;
 import africa.semicolon.blogApplication.data.dtos.response.PostResponseDto;
-import africa.semicolon.blogApplication.data.exceptions.PostDoesNotExistException;
-import africa.semicolon.blogApplication.data.models.Author;
-import africa.semicolon.blogApplication.data.models.Comment;
 import africa.semicolon.blogApplication.data.models.Post;
 import africa.semicolon.blogApplication.data.repositories.PostRepository;
 import africa.semicolon.blogApplication.data.repositories.PostRepositoryImpl;
 import africa.semicolon.blogApplication.utils.PostModelMapper;
-
-import java.time.LocalDateTime;
-import java.util.List;
 
 public class PostServiceImpl implements  PostService{
 
@@ -27,15 +21,18 @@ public class PostServiceImpl implements  PostService{
         return responseDto;
     }
 
-    @Override
-    public List<Post> findAByPost(PostRequestDto postRequestDto) {
-        return null;
-    }
 
     @Override
-    public PostResponseDto updatePost(PostRequestDto postRequestDto) {
+    public Post updatePost(String id, PostRequestDto postRequestDto) {
 
-        return null;
+        Post postToUpdate = findPostById(id);
+        if (!postToUpdate.getTitle().equals(postRequestDto.getTitle())){
+            postToUpdate.setTitle(postRequestDto.getTitle());
+        }
+        if (!postToUpdate.getContent().equals(postRequestDto.getContent())){
+            postToUpdate.setContent(postRequestDto.getContent());
+        }
+        return postRepository.save(postToUpdate);
     }
 
     @Override
@@ -44,8 +41,9 @@ public class PostServiceImpl implements  PostService{
     }
 
     @Override
-    public void deletePostById(long id) {
-
+    public void deletePostById(String id) {
+        Post findAPostId = postRepository.findByPostId(id);
+        postRepository.delete(findAPostId);
     }
 
 }
