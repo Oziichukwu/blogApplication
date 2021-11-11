f2a78bc1b31ce6572320b4438d26bfad367bcb23
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-11 07:57:01 +0100

    repositories updated

--- src/test/java/africa/semicolon/blogApplication/data/repositories/CommentRepositoryImplTest.java
+++ src/test/java/africa/semicolon/blogApplication/data/repositories/CommentRepositoryImplTest.java
@@ -1,4 +1,78 @@
+package africa.semicolon.blogApplication.data.repositories;
+
+import africa.semicolon.blogApplication.data.models.Comment;
+import africa.semicolon.blogApplication.data.models.Post;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CommentRepositoryImplTest {
-  
+
+    CommentRepository commentRepository;
+    @BeforeEach
+    void setUp() {
+        commentRepository = new CommentRepositoryImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void save() {
+        Comment comment = new Comment();
+        Post post = new Post();
+        comment.setCommentId("321011");
+        comment.setPostId(post.getPostId());
+        comment.setCommentorName("Precious Chidiebere");
+        comment.setContent("Its high time africa begin to harness this great potential");
+        Comment savedComment = commentRepository.save(comment);
+        assertEquals(comment, savedComment);
+        assertEquals(1, commentRepository.findAll().size());
+
+    }
+    public Comment savedCommetTest(){
+        Comment comment = new Comment();
+        Post post = new Post();
+        comment.setCommentId("321011");
+        comment.setPostId(post.getPostId());
+        comment.setCommentorName("Precious Chidiebere");
+        comment.setContent("Its high time africa begin to harness this great potential");
+        return commentRepository.save(comment);
+
+    }
+
+    @Test
+    void findByUniqueId() {
+        Comment savedComment = savedCommetTest();
+        assertEquals(savedComment, commentRepository.findByUniqueId(savedComment.getCommentId()).get());
+    }
+
+    @Test
+    void delete() {
+        Comment savedComment = savedCommetTest();
+        assertEquals(1, commentRepository.findAll().size());
+        commentRepository.delete(savedComment);
+        assertEquals(0, commentRepository.findAll().size());
+    }
+
+    @Test
+    void testDelete() {
+        Comment savedComment = savedCommetTest();
+        assertEquals(1, commentRepository.findAll().size());
+        commentRepository.delete(savedComment);
+        assertEquals(0, commentRepository.findAll().size());
+    }
+
+    @Test
+    void deleteAll() {
+        savedCommetTest();
+        assertEquals(1, commentRepository.findAll().size());
+        commentRepository.deleteAll();
+        assertEquals(0, commentRepository.findAll().size());
+    }
 }
--- src/test/java/africa/semicolon/blogApplication/data/repositories/PostRepositoryImplTest.java
+++ src/test/java/africa/semicolon/blogApplication/data/repositories/PostRepositoryImplTest.java
@@ -1,4 +1,82 @@
+package africa.semicolon.blogApplication.data.repositories;
+
+import africa.semicolon.blogApplication.data.models.Author;
+import africa.semicolon.blogApplication.data.models.Post;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PostRepositoryImplTest {
-  
+
+    PostRepository postRepository;
+
+    @BeforeEach
+    void setUp() {
+        postRepository = new PostRepositoryImpl();
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void save() {
+        Author author = new Author();
+        Post post = new Post();
+        post.setAuthorId(author.getAuthorId());
+        post.setPostId("32101");
+        post.setTitle("Importance of Tech Innovations in Africa");
+        post.setContent("The importance of tech innovation in africa cannot be overemphasized" +
+                "as the economic growth of the continent depends largely on innovations in the area of tech");
+
+        Post savedPost = postRepository.save(post);
+        assertEquals(1, postRepository.findAll().size());
+        assertEquals(savedPost,savedPost);
+    }
+
+    public Post postTest(){
+        Author author = new Author();
+        Post post = new Post();
+        post.setAuthorId(author.getAuthorId());
+        post.setPostId("32101");
+        post.setTitle("Importance of Tech Innovations in Africa");
+        post.setContent("The importance of tech innovation in africa cannot be overemphasized" +
+                "as the economic growth of the continent depends largely on innovations in the area of tech");
+
+        return postRepository.save(post);
+    }
+
+    @Test
+    void findPostByPostId() {
+        Post savedPost = postTest();
+        assertEquals(savedPost, postRepository.findPostByPostId(savedPost.getPostId()).get());
+    }
+
+    @Test
+    void delete() {
+        Post savedPost = postTest();
+        assertEquals(1, postRepository.findAll().size());
+        postRepository.delete(savedPost);
+        assertEquals(0, postRepository.findAll().size());
+    }
+
+    @Test
+    void testDelete() {
+        Post savedPost = postTest();
+        assertEquals(1, postRepository.findAll().size());
+        postRepository.delete(savedPost.getPostId());
+        assertEquals(0, postRepository.findAll().size());
+    }
+
+    @Test
+    void deleteAll() {
+        postTest();
+        assertEquals(1, postRepository.findAll().size());
+        postRepository.deleteAll();
+        assertEquals(0, postRepository.findAll().size());
+    }
+
 }
