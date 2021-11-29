d209a367d4325368147044ba802e896dbc71c6c5
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-29 09:05:34 +0100

    blog update

--- src/main/java/africa/semicolon/blogApplication/services/AuthorService.java
+++ src/main/java/africa/semicolon/blogApplication/services/AuthorService.java
@@ -1,17 +1,16 @@
 package africa.semicolon.blogApplication.services;
 
-import africa.semicolon.blogApplication.data.dtos.request.AuthorRequestDto;
-import africa.semicolon.blogApplication.data.dtos.response.AuthorResponseDto;
+import africa.semicolon.blogApplication.data.dtos.request.CreateAuthorRequest;
+import africa.semicolon.blogApplication.data.dtos.response.CreateAuthorResponse;
 import africa.semicolon.blogApplication.data.models.Author;
 
 import java.util.List;
 
 public interface AuthorService {
 
-    AuthorResponseDto createAuthor(AuthorRequestDto authorRequestDto);
-    Author updateAuthor(String id, AuthorRequestDto authorRequestDto);
-    List<Author>findById();
+    CreateAuthorResponse createAuthor(CreateAuthorRequest authorRequestDto);
+    Author updateAuthor(String authorId, CreateAuthorRequest authorRequestDto);
+    List<Author>findAllAuthors();
     void deleteAuthorById(String id);
-    void deleteAuthor(Author author);
     Author findAuthorById(String id);
 }
